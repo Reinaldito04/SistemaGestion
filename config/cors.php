cdf2992e736eca32825732fd1f665dcca73a379e
JATN1506 <jt7471934@gmail.com>
2025-07-14 20:47:29 -0400

    reversado cors

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // 🔓 Permite desde cualquier origen
+    'allowed_origins' => ['http://localhost:3000'], // Reemplaza por tu frontend si es otro
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, // ⚠️ Importante: debe estar en false si usas '*'
+    'supports_credentials' => true,
 ];
